97488813886c6a49bff7912f01f8f84b5cec6abf
Myo Min Latt <myominlatt.sbs@gmail.com>
2024-07-09 10:12:38 +0630

    Firebase messaging > UAT ONLY

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'dart:async';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpodapisample/firebase_api.dart';
+import 'package:riverpodapisample/firebase_options.dart';
 import 'core/feat_core.dart';
 
 FutureOr<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await FirebaseApi().initNotifications();
   runApp(ProviderScope(child: AppWidget()));
 }
